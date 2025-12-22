0d30e048d65b1f04d6bd91bcc07ad5b2a44c90d6
kolunio <10465218-kolunio@users.noreply.gitlab.com>
2025-12-22 14:35:24 +0100

    add dataset class

--- model.py
+++ model.py
@@ -37,16 +37,111 @@ def prepare_musdb_index(input_path, output_path, duration, overlap):
                 t_end = t_start + frames
                 print(f'{track.path};{t_start};{t_end}', file=w)
                 print(f'{track.path};{t_start};{t_end}')
+
+class MUSDB18Dataset(torch.utils.data.Dataset):
+    def __init__(self, dataset_index_path, n_fft, amplitude_only):
+        lines = []
+        with open(dataset_index_path, 'r') as r:
+            lines = r.readlines()
+            
+        def line_to_fragment(line):
+            [track, start, end] = line.split(';')
+            return track, int(start), int(end)
+        
+        self.fragments = list(map(line_to_fragment, lines))
+        self.n_fft = n_fft
+        self.window = torch.hann_window(self.n_fft, periodic=True, dtype=torch.float32)
+        self.amplitude_only = amplitude_only
+        
+    
+    def __getitem__(self, index):
+        track, start, end = self.fragments[index]
+        
+        def get(sub_path):
+            samples, sample_rate = torchaudio.load(
+                os.path.join(track, sub_path),
+                frame_offset = start, 
+                num_frames = end - start, 
+                normalize = True, 
+                channels_first = True
+            )
+            assert sample_rate == 44100
+        
+            return torch.stft(samples, n_fft=self.n_fft, return_complex=True, window=self.window)
+            
+            
+            if self.amplitude_only:
+                return spectrum_abs 
+            else:
+                return spectrum
+                
+        full = get('./mixture.wav')
+        vocal = get('./vocals.wav')
+        
+        full_abs  = full.abs()
+        vocal_abs = vocal.abs()
+
+        full_max = full_abs.max()
+        
+        if self.amplitude_only:
+            return full_abs / full_max, vocal_abs / full_max
+        else:
+            return full / full_max, vocal / full_max
+        
+    def __len__(self):
+        return len(self.fragments)
+    
+        
+
+
+train_path = './train_dataset.csv'
+test_path = './test_dataset.csv'
+
+
+
+
     
 match sys.argv:
     case [_, 'prepare']:
         duration = 4
         overlap  = 2
-        prepare_musdb_index('./musdb/train/', './train_index.csv', duration, overlap)
-        prepare_musdb_index('./musdb/test/', './test_index.csv',   duration, overlap)        
+        prepare_musdb_index('./musdb/train/', train_path, duration, overlap)
+        prepare_musdb_index('./musdb/test/',  test_path,  duration, overlap)      
+    case [_, 'load_test']:
+        n_fft = 1024
+        
+        waveform = torch.zeros((2, 44100 * 400), dtype=torch.float32)
+        weights = torch.zeros((1, 44100 * 400),  dtype=torch.float32)
+        
+        
+        musdb = MUSDB18Dataset(train_path, n_fft, False)
+        
+        current_track = None
+        for i, (x, y) in enumerate(musdb):
+            x *= 500
+            y *= 500
+            
+            track, start, end = musdb.fragments[i]
+            fragment = torch.istft(y, n_fft=musdb.n_fft, window=musdb.window, center=True)
+      
+            print(track, start, end)
+      
+            if current_track == None: current_track = track
+            if current_track != track:
+                current_track = track
+                break
+            
+            waveform[:, start:start+fragment.shape[1]] += fragment[:]
+            weights[:, start:start+fragment.shape[1]] += 1
+                         
+        
+        waveform /= weights
+        
+        torchaudio.save('load_test.wav', waveform, 44100)
     case _:
-        print('unknown command')
-        print('usage:')
-        print('  prepare | Prepares the train and test indexes from ./musdb/train and ./musdb/test directories')
+        print(f'unknown command')
+        print(f'usage:')
+        print(f'  prepare   | Prepares the train and test dataset indexes from ./musdb/train and ./musdb/test directories')
+        print(f'  load_test | Reconstructs first track as specified in train dataset index to load_test.wav' )
         sys.exit(1)
         
