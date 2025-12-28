914015c727d54d6b10901d1152cec66b48151165
kolunio <10465218-kolunio@users.noreply.gitlab.com>
2025-12-28 10:32:47 +0100

    add BetterConv model (it overfits)

--- model.py
+++ model.py
@@ -486,6 +486,88 @@ class WideConv(nn.Module):
         return x
 
 
+class BetterConv(nn.Module):
+    def __init__(self, sample_rate, frequency_bin_count):
+        super(self.__class__, self).__init__()
+        self.conv2d_1 = nn.Conv2d(in_channels=2, out_channels=12, kernel_size=5, padding=2)
+        self.bn1 = nn.BatchNorm2d(12)
+        self.max_pool2d_1 = nn.MaxPool2d(kernel_size=(5, 5), stride=(5, 5))
+
+
+        self.conv2d_2 = nn.Conv2d(in_channels=12, out_channels=20, kernel_size=5, padding=2)
+        self.bn2 = nn.BatchNorm2d(20)
+        self.max_pool2d_2 = nn.MaxPool2d(kernel_size=(1, 5), stride=(1, 5))
+
+        self.conv2d_3 = nn.Conv2d(in_channels=20, out_channels=40, kernel_size=5, padding=2)
+        self.bn3 = nn.BatchNorm2d(40)
+        self.conv2d_4 = nn.Conv2d(in_channels=40, out_channels=30, kernel_size=5, padding=2)
+        self.bn4 = nn.BatchNorm2d(30)
+        self.conv2d_5 = nn.Conv2d(in_channels=30, out_channels=20, kernel_size=5, padding=2)
+        self.bn5 = nn.BatchNorm2d(20)
+
+        self.conv2d_6 = nn.Conv2d(in_channels=20*2, out_channels=12, kernel_size=5, padding=2)
+        self.bn6 = nn.BatchNorm2d(12)
+
+        self.conv2d_7 = nn.Conv2d(in_channels=12*2, out_channels=2, kernel_size=5,  padding=2)
+
+        self.lstm = nn.LSTM(input_size=frequency_bin_count//25, hidden_size=64, batch_first=True, bidirectional=True)
+        self.fc = nn.Linear(128, frequency_bin_count//25)
+
+    def forward(self, x):
+        def gate(x, skip):
+            return torch.cat((x, skip), dim=1)
+
+        batches, channels, frequencies, timesteps = x.shape
+
+
+        x = x.transpose(2, 3)                                                # [batches, 2, timesteps, frequencies]
+        saved_2_channel = x
+
+        x = self.conv2d_1(x)                                                 # [batches, 12, timesteps, frequency_bin_count]
+        x = self.bn1(x)
+        saved_12_channel = x
+        x = nn.functional.gelu(x)
+        x = self.max_pool2d_1(x)                                             # [batches, 12, timesteps/3, frequency_bin_count/3]
+
+        x = self.conv2d_2(x)                                                 # [batches, 20, timesteps/3, frequency_bin_count/9]
+        x = self.bn2(x)
+        saved_20_channel = x
+        x = nn.functional.gelu(x)
+        x = self.max_pool2d_2(x)                                             # [batches, 20, timesteps/3, frequency_bin_count/9]
+
+        x = self.conv2d_3(x)                                                 # [batches, 40, timesteps/3, frequency_bin_count/9]
+        x = self.bn3(x)
+        x = nn.functional.gelu(x)
+        x = self.conv2d_4(x)                                                 # [batches, 30, timesteps/3, frequency_bin_count/9]
+        x = self.bn4(x)
+        x = nn.functional.gelu(x)
+
+        batches, channels, steps, freqs = x.shape
+        x = x.reshape(batches * channels, steps, freqs)
+        x, _  = self.lstm(x)
+        x = nn.functional.gelu(x)
+        x = self.fc(x)
+        x = x.reshape(batches, channels, steps, freqs)
+
+
+        x = self.conv2d_5(x)                                                 # [batches, 20, timesteps/3, frequency_bin_count/9]
+        x = self.bn5(x)
+        x = nn.functional.gelu(x)
+        x = nn.functional.interpolate(x, scale_factor=(1, 5), mode='nearest')# [batches, 20, timesteps/3, frequency_bin_count/3]
+        x = gate(x, saved_20_channel)
+
+        x = self.conv2d_6(x)                                                 # [batches, 12, timesteps/3, frequency_bin_count/3]
+        x = self.bn6(x)
+        x = nn.functional.gelu(x)
+        x = nn.functional.interpolate(x, scale_factor=(5, 5), mode='nearest')# [batches, 12, timesteps,   frequency_bin_count]
+        x = gate(x, saved_12_channel)
+
+        x = self.conv2d_7(x)                                                 # [batches  2,  timesteps,   frequency_bin_count]
+        x = nn.functional.gelu(x)
+        x = x * torch.sigmoid(saved_2_channel)
+        x = x.transpose(2, 3)                                                # [batches, 2, frequencies, timesteps]
+
+        return x
 
 
     
